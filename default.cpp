8ee47d433f72241f09000f863a351d600e73bc63
Rerko2 <rerko2.yt@gmail.com>
2025-04-07 10:41:51 +0200

    Update 1 HRA.cpp

--- default.cpp
+++ default.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main(){
-
+int a,b;
+cout << "výtej v této dobrodružné hře! ";
+cout << "\nJaké je tvé jmáno dobrodruhu? ";
+cin >> a
+cout << "\n\nZajmavé jméno, opravdu zajmavé. No tak se vydáme na toto dobrodružství" << a << " ";
+cout << "Dostali jsme se na křižovatku levý směr vede do lesa, pravý do malé vesničky" << "\nKam se vydáme? [P/L]";
+if(b==P){ cout << "Zajmavé rozhodnutí, jdeme do vesničky";
+  }else{ cout << "Zajmavé rozhodnutí, jdeme do lesa";
+    }
 }
